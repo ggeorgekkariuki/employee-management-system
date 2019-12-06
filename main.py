f694db7698d02b25c7f9a2b8a81f682acada9c5b
George Gachugu Kariuki <gkariuki@ihlkenya.co.ke>
2019-12-06 11:40:55 +0300

    Import Employee class

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from flask import Flask
 
+# Import the configurations
+from config.configs import Development
+
+# Import the Employee class that will be used to generate a payroll
+from resources.employee import Employee
+
 app = Flask(__name__)
 
+# Set up the configuration with the Flask app above
+app.config.from_object(Development)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
